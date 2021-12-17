6ebe3d0f05be96beb3ad37461dd4e86dd354a926
Abi M <abigail.manoguid.lia@gmail.com>
2021-12-17 13:42:39 +0800

    2nd commit

---
+++ src/Login.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Login {
+    private JTextPane textPane1;
+
+    private JPanel Window;
+    private JLabel Title;
+    private JPanel title_Components;
+    private JPanel username_Components;
+    private JPanel password_Components;
+    private JTextField username_Input;
+    private JLabel username_Label;
+    private JPasswordField password_Input;
+    private JLabel password_Label;
+    private JPanel button_Components;
+    private JButton login_Button;
+    private JLabel app_Icon;
+
+
+    public void setVisible(boolean b) {
+
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        Login test = new Login();
+        test.setVisible(true);
+    }
+}
